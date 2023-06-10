fde6124097d9141a9c59d19f7f89321ac62c57bc
Santiago-Cajachagua <santiagocolabora4@gmail.com>
2023-06-10 15:10:55 -0500

    Tercer commit con ya las ultimas

---
+++ app/src/main/java/com/practicas/proyectomovildeliverygo/activities/Activity_Tus_Pedidos.kt
@@ -0,0 +1,12 @@
+package com.practicas.proyectomovildeliverygo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practicas.proyectomovildeliverygo.R
+
+class Activity_Tus_Pedidos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tus_pedidos)
+    }
+}
