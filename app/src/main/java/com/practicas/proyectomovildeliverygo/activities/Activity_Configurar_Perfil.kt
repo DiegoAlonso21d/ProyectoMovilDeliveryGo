5a5d4347d8d404c4545034d77032bcb1f0452e46
Santiago-Cajachagua <santiagocolabora4@gmail.com>
2023-06-11 18:10:46 -0500

    posible ultimo commit

---
+++ app/src/main/java/com/practicas/proyectomovildeliverygo/activities/Activity_Configurar_Perfil.kt
@@ -0,0 +1,12 @@
+package com.practicas.proyectomovildeliverygo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practicas.proyectomovildeliverygo.R
+
+class Activity_Configurar_Perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configurar_perfil)
+    }
+}
