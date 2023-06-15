a203027e972fa3b30b3a2ac72b0da504bf005d94
IsaacDelevop <isaac.mit254@gmail.com>
2023-06-14 19:15:44 -0500

    listadoBusquedaBebidas

---
+++ app/src/main/java/com/practicas/proyectomovildeliverygo/activities/BuscarProductoBebidas.kt
@@ -0,0 +1,12 @@
+package com.practicas.proyectomovildeliverygo.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.practicas.proyectomovildeliverygo.R
+
+class BuscarProductoBebidas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buscar_producto)
+    }
+}
